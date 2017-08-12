e33d660ae5f0fe6492d07ca52356db7dda23bdf1
ttt <ttt@ALE-554RL5K>
2017-08-11 21:38:17 -0400

    Created the Splash Form

---
+++ COMP123-Summer2017-Lesson12B2/SplashForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Name: Alessandro da Silva Santos
+ * Student Number: 300927354
+ * Date: August 8,2017.
+ * Description: Splash Form class
+ * Version 0.1- Created the Splash Form 
+ */
+
+namespace COMP123_Summer2017_Lesson12B2
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
